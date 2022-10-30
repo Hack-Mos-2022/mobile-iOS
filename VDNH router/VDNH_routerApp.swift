561590b127a15d3bdf19647a114f9757a6fd14b1
Anatoliy Kant <kant.anatoliy@yandex.ru>
2022-10-30 14:46:40 +0400

    Initial Commit

---
+++ VDNH router/VDNH_routerApp.swift
@@ -0,0 +1,17 @@
+//
+//  VDNH_routerApp.swift
+//  VDNH router
+//
+//  Created by Anatoliy Podkladov on 30.10.2022.
+//
+
+import SwiftUI
+
+@main
+struct VDNH_routerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
